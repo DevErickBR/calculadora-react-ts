208e0fa1eebfc08bf325ee2392bbf38587a86537
DevErickBR <erickyanSP@outlook.com>
2022-10-18 19:07:48 -0300

    add comunication app > keyBoard

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,24 @@ import { KeyBoard } from "./components/KeyBoard";
 function App() {
   const [firstValue, setFirstValue] = useState<number>(0);
   const [secondValue, setSencondValue] = useState<number>(0);
-  const [currentValue, setCurrentValue] = useState<number>(0);
+  const [currentValue, setCurrentValue] = useState<string>("0");
+  const handleSetCurrentValue = (e: string) => {
+    if (currentValue == "0") {
+      return setCurrentValue(e)
+    } else {
+      return setCurrentValue(currentValue + e)
+    }
+  }
+
+  handleSetCurrentValue
   return (
     <div>
       <div>
         {/* <Display value={currentValue} /> */}
       </div>
       <div>
-        <KeyBoard />
+        <KeyBoard setValue={handleSetCurrentValue} />
+        <div>{currentValue}</div>
       </div>
     </div>
   );
--- src/components/KeyBoard/index.tsx
+++ src/components/KeyBoard/index.tsx
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
+type Props = {
+    setValue: (e: string) => void;
+}
 
-export const KeyBoard = () => {
-    const [value, setValue] = useState<string>("0")
-    const handleClick = (e: string) => {
-        if (value == "0") {
-            return setValue(e)
-        } else {
-            return setValue(value + e)
-        }
-    }
+export const KeyBoard = ({ setValue }: Props) => {
+    // const [value, setValue] = useState<string>("0")
+    // const handleClick = (e: string) => {
+    //     if (value == "0") {
+    //         return setValue(e)
+    //     } else {
+    //         return setValue(value + e)
+    //     }
+    // }
 
     return (
         <div>
-            <input type="text" value={value} />
-            <button onClick={() => handleClick("9")}>9</button>
-            <button onClick={() => handleClick("8")}>8</button>
-            <button onClick={() => handleClick("7")}>7</button>
-            <button onClick={() => handleClick("6")}>6</button>
-            <button onClick={() => handleClick("5")}>5</button>
-            <button onClick={() => handleClick("4")}>4</button>
-            <button onClick={() => handleClick("3")}>3</button>
-            <button onClick={() => handleClick("2")}>2</button>
-            <button onClick={() => handleClick("1")}>1</button>
-            <button onClick={() => handleClick("0")}>0</button>
-            <button onClick={() => handleClick(",")}>,</button>
+            <button onClick={() => setValue("9")}>9</button>
+            <button onClick={() => setValue("8")}>8</button>
+            <button onClick={() => setValue("7")}>7</button>
+            <button onClick={() => setValue("6")}>6</button>
+            <button onClick={() => setValue("5")}>5</button>
+            <button onClick={() => setValue("4")}>4</button>
+            <button onClick={() => setValue("3")}>3</button>
+            <button onClick={() => setValue("2")}>2</button>
+            <button onClick={() => setValue("1")}>1</button>
+            <button onClick={() => setValue("0")}>0</button>
+            <button onClick={() => setValue(",")}>,</button>
         </div>
     );
 };
